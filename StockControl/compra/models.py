23dcb554ea8de48ba313a50ee8f4e3701e849ce2
Mauricio Mamani <maurobvx@gmail.com>
2023-05-17 19:26:11 -0300

    Mejorando codigo

--- StockControl/compra/models.py
+++ StockControl/compra/models.py
@@ -6,7 +6,7 @@ class Proveedor(models.Model):
 
     nombre = models.CharField(max_length=50,default="");
     apellido = models.CharField(max_length=50,default="");
-    dni = models.IntegerField(max_length=10, default=0);
+    dni = models.IntegerField(default=0);
     
     def __str__(self):
         """Retorna una cadena con los campos nombre y apellido del modelo Proveedor"""
@@ -16,7 +16,7 @@ class Proveedor(models.Model):
 
 class Producto(models.Model):
     """Crea un modelo de la entidad Producto."""
-    
+
     nombre = models.CharField(max_length=50, default="")
     precio = models.FloatField(default=0)
     stock_actual = models.IntegerField(default=0)
